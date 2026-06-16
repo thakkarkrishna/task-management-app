1667dc69c301b1468515541d7a51af2cc3d1a891
thakkarkrishna <krishnakrishna85792@gmail.com>
2026-06-16 13:27:15 +0530

    Added home route

--- server/App.js
+++ server/App.js
@@ -3,6 +3,7 @@ dotenv.config();
 
 import express from "express";
 import connectDB from "./config/db.js";
+import taskRoutes from "./routes/taskroutes.js";
 
 const app = express();
 
@@ -11,6 +12,12 @@ app.use(express.json());
 connectDB(process.env.MONGO_URI);
 
 const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  res.send("Task Management API is running 🚀");
+});
+
+app.use("/api/tasks", taskRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server Running on ${PORT}`);
